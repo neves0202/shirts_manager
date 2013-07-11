206a1f0e6d5dfad0586a765378f093c44a0d7a81
Neves <neves0202@gmail.com>
2013-07-11 19:59:38 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShirtsManager::Application.config.secret_key_base = '44ad41c0b7bfae0dc4f3af98ceab4c02ad528621dba6772215c0df3c38f6472905a1bc26cd5cc7aa586e94067bbe40677e137f45655c28d983a3dba20bb50409'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShirtsManager::Application.config.session_store :cookie_store, key: '_shirts_manager_session'
